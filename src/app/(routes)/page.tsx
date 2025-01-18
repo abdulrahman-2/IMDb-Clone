07091de5d6d46ec48abb6ed1644b5b8aedfa1160
abdulrahman-2 <abdoattallah3@gmail.com>
2025-01-18 15:14:20 +0200

    add pages Pagination

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,19 +1,27 @@
+import Pagination from "@/components/Pagination";
 import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY;
-const Home = async () => {
+
+const Home = async ({ searchParams }: { searchParams: { page: string } }) => {
+  const page = searchParams.page || "1";
+
   const res = await fetch(
-    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=${page}`
   );
   const data = await res.json();
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+
   const results = data.results;
+  const totalPages = data.total_pages;
+
   return (
     <div>
       <Results results={results} />
+      <Pagination currentPage={parseInt(page)} totalPages={totalPages} />
     </div>
   );
 };
