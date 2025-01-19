18ac425d1b0f02f3bfcea8909c8b2b7cd2d32bf7
abdulrahman-2 <abdoattallah3@gmail.com>
2025-01-19 15:19:51 +0200

    add favicon

--- src/app/api/user/fav/route.ts
+++ src/app/api/user/fav/route.ts
@@ -1,32 +1,38 @@
 import User from "@/lib/models/user.model";
 import { connectDb } from "@/lib/mongodb/mongoose";
 import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export const PUT = async (req: Request) => {
   const user = await currentUser();
   const client = await clerkClient();
+
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
   try {
     await connectDb();
     const data = await req.json();
-    if (!user) {
-      return { status: 401, body: "Unauthorized" };
-    }
+
     const existingUser = await User.findById(user.publicMetadata.userMongoId);
-    if (existingUser.favs.some((fav: any) => fav.movieId === data.movieId)) {
-      const updatedUser = await User.findByIdAndUpdate(
+    if (!existingUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    const isMovieInFavs = existingUser.favs.some(
+      (fav: any) => fav.movieId === data.movieId
+    );
+
+    let updatedUser;
+    if (isMovieInFavs) {
+      updatedUser = await User.findByIdAndUpdate(
         user.publicMetadata.userMongoId,
         { $pull: { favs: { movieId: data.movieId } } },
         { new: true }
       );
-      const updatedFavs = updatedUser.favs.map((fav: any) => fav.movieId);
-      await client.users.updateUserMetadata(user.id, {
-        publicMetadata: {
-          favs: updatedFavs,
-        },
-      });
-      return new Response(JSON.stringify(updatedUser), { status: 200 });
     } else {
-      const updatedUser = await User.findByIdAndUpdate(
+      updatedUser = await User.findByIdAndUpdate(
         user.publicMetadata.userMongoId,
         {
           $addToSet: {
@@ -42,16 +48,22 @@ export const PUT = async (req: Request) => {
         },
         { new: true }
       );
-      const updatedFavs = updatedUser.favs.map((fav: any) => fav.movieId);
-      await client.users.updateUserMetadata(user.id, {
-        publicMetadata: {
-          favs: updatedFavs,
-        },
-      });
-      return new Response(JSON.stringify(updatedUser), { status: 200 });
     }
+
+    const updatedFavs = updatedUser.favs.map((fav: any) => fav.movieId);
+    await client.users.updateUserMetadata(user.id, {
+      publicMetadata: { favs: updatedFavs },
+    });
+
+    return NextResponse.json(
+      { message: "Favorites updated", data: updatedUser },
+      { status: 200 }
+    );
   } catch (error) {
-    console.log("Error adding favs to the user:", error);
-    return new Response("Error adding favs to the user", { status: 500 });
+    console.error("Error updating favorites:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 };
